537474d25a269301b65c8cd283a00be3f75a3ca7
Al Johri <al.johri@gmail.com>
2014-11-28 16:47:32 -0600

    add flask app
    add host='0.0.0.0'
    
    read port from env var
    
    indentation error

--- canvas.py
+++ canvas.py
@@ -1,52 +1,71 @@
-import requests, base64, lxml.html, os, pickle, logging
+import requests, base64, lxml.html, os, pickle, logging, datetime
 
 logging.getLogger("requests").setLevel(logging.DEBUG)
 
-if not os.path.isfile("canvas.pickle"):
-  s = requests.Session()
-  response = s.get("https://canvas.northwestern.edu")
-  login_payload = {
-    "IDToken0": "",
-    "IDToken1": os.getenv('NETID'),
-    "IDToken2": os.getenv('PASSWORD'),
-    "IDButton": "Log In",
-    "goto": base64.b64encode("https://fed.it.northwestern.edu/idp/Authn/RemoteUser"),
-    "SunQueryParamsString": "",
-    "encoded": "true",
-    "gx_charset": "UTF-8"
-  }
-  response = s.post("https://websso.it.northwestern.edu/amserver/UI/Login", data=login_payload)
-  doc = lxml.html.fromstring(response.content)
-  saml_payload = {
-    "SAMLResponse": doc.cssselect("input[name=SAMLResponse]")[0].get('value')
-  }
-  response = s.post("https://canvas.northwestern.edu/saml_consume", data=saml_payload)
-  response = s.get("https://canvas.northwestern.edu/courses/3107/modules/items/28628")
-  doc = lxml.html.fromstring(response.content)
-  codecritic = doc.cssselect("#tool_form")[0]
-  codecritic_payload = {i.get('name'):i.get('value') for i in codecritic.cssselect("input")}
-  response = s.post("https://lyonesse.cs.northwestern.edu:8443/Submitter/", data=codecritic_payload)
-  pickle.dump(s, open("canvas.pickle", "wb"))
-else:
-  s = pickle.load(open("canvas.pickle", "rb"))
-
 SESSION_EXPIRED = "Your Code Critic session has been closed."
+PASSWORD_INVALID = "The NetID and/or password you entered was invalid."
+
 state = lambda response, state_string: state_string in response.text
 
-response = s.get("https://lyonesse.cs.northwestern.edu:8443/Submitter/student/reviewer.do")
-doc = lxml.html.fromstring(response.content)
+def auth(override=False):
+  if not os.path.isfile("canvas.pickle") or override:
+    print "DEBUG", "redownloading session"
+    s = requests.Session()
+    response = s.get("https://canvas.northwestern.edu")
+    login_payload = {
+      "IDToken0": "",
+      "IDToken1": os.getenv('NETID'),
+      "IDToken2": os.getenv('PASSWORD'),
+      "IDButton": "Log In",
+      "goto": base64.b64encode("https://fed.it.northwestern.edu/idp/Authn/RemoteUser"),
+      "SunQueryParamsString": "",
+      "encoded": "true",
+      "gx_charset": "UTF-8"
+    }
+    response = s.post("https://websso.it.northwestern.edu/amserver/UI/Login", data=login_payload)
+    if state(response, PASSWORD_INVALID):
+      raise Exception(PASSWORD_INVALID + "\nYou might be missing the NETID and PASSWORD environment variables.")
+    doc = lxml.html.fromstring(response.content)
+    saml_payload = {
+      "SAMLResponse": doc.cssselect("input[name=SAMLResponse]")[0].get('value')
+    }
+    response = s.post("https://canvas.northwestern.edu/saml_consume", data=saml_payload)
+    response = s.get("https://canvas.northwestern.edu/courses/3107/modules/items/28628")
+    doc = lxml.html.fromstring(response.content)
+    codecritic = doc.cssselect("#tool_form")[0]
+    codecritic_payload = {i.get('name'):i.get('value') for i in codecritic.cssselect("input")}
+    response = s.post("https://lyonesse.cs.northwestern.edu:8443/Submitter/", data=codecritic_payload)
+    pickle.dump(s, open("canvas.pickle", "wb"))
+  else:
+    print "DEBUG", "using cached session"
+    s = pickle.load(open("canvas.pickle", "rb"))
+  return s
 
-queue_size = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[0].text_content().split("; ")[-1].replace(" (all)", "")
-you_can_submit = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[1].text_content()
-most_recent_review_time = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[2].text_content()
+def get_page(s):
+  return s.get("https://lyonesse.cs.northwestern.edu:8443/Submitter/student/reviewer.do")  
 
-print queue_size
-print you_can_submit
-print most_recent_review_time
+def parse_page(response):
+    doc = lxml.html.fromstring(response.content)
+    queue_size = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[0].text_content().split("; ")[-1].replace(" (all)", "")
+    you_can_submit = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[1].text_content().replace("\r\n", "").replace(" ", "")
+    most_recent_review_time = doc.cssselect("h2")[0].getnext().cssselect("tr:nth-child(3) th")[2].text_content()
+    return (queue_size, you_can_submit, most_recent_review_time)
 
-if state(response, SESSION_EXPIRED):
-  s = None
-  os.remove("canvas.pickle")
+def check_queue(s=None):
+  print "holler"
+  if not s: s = auth()
+  response = get_page(s)
 
-with open("test.html", "w") as f:
-  f.write(response.text.encode('utf-8'))
+  while state(response, SESSION_EXPIRED):
+    os.remove("canvas.pickle")
+    s = auth(override=False)
+    response = get_page()
+
+  queue_size, you_can_submit, most_recent_review_time = parse_page(response)
+
+  print "DEBUG", (queue_size, you_can_submit, most_recent_review_time)
+
+  return (queue_size, you_can_submit, most_recent_review_time)
+
+if __name__ == "__main__":
+    check_queue()
